1954446a192a63a956a79576bebcb5345f81e2d6
Aaron Yordanyan <aaron.yor@gmail.com>
2023-04-07 10:32:26 +0400

    Implement Environment variables passing logic

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -132,6 +132,35 @@ const config = {
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'QTLOXHXTI5',
+
+        // Public API key: it is safe to commit it
+        apiKey: 'f621c5bc34adae4dee4f97d8536c31e5',
+
+        indexName: 'entkit_docs',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
+      },
     }),
 };
 
